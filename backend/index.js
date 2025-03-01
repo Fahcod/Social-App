6fc491a5beecd803dfb4e73e952fec2ad0b1c9b2
Fahcod <twesigyefahad94@gmail.com>
2025-03-02 01:10:06 +0300

    Added the frontend url

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     methods:["POST","GET","PUT","DELETE"],
-    origin:"http://localhost:5173",
+    origin:"https://socialspot-app.onrender.com",
     credentials:true
 }));
 
