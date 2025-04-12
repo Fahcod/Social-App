f38423afef6b592c5d8fca2e1ae9686a1cf1254e
Fahcod <twesigyefahad94@gmail.com>
2025-04-12 04:29:56 +0300

    Added scroll func on friend seide bar

--- frontend/src/pages/ChatPage/Sidebar/Sidebar.jsx
+++ frontend/src/pages/ChatPage/Sidebar/Sidebar.jsx
@@ -11,7 +11,7 @@ const SideFriends = () => {
 
   return (
     <div className={`w-[100%] z-[100] duration-700 ${shower?`translate-x-0`:`translate-x-[100%] md:translate-x-0`} md:z-[unset] md:w-[25%] fixed flex  bg-white dark:bg-dark h-screen border-solid border-r-[1px] border-gray-200 dark:border-[#333]`}>
-    <div className="w-full pt-3 flex flex-col gap-3 px-3">
+    <div className="w-full pt-3 flex flex-col gap-3 px-3 h-screen overflow-y-auto [&::-webkit-scrollbar]:hidden md:pb-36 pb-24 ">
      {users.map((item,index)=>{
         if(friendList.includes(item._id)){
         return <ChatItem key={index} _id={item._id} user_bio={item.user_bio} profile={item.profile} username={item.username}/>
